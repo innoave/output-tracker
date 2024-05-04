41d93b2aaae05f44c63e66eccc63cbe6ad46bf84
haraldmaida <harald.maida@innoave.com>
2024-05-04 23:09:27 +0200

    configure Compiler and Clippy lints in Cargo.toml instead of lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,3 @@
-#![deny(unsafe_code, unstable_features)]
-#![warn(
-    bare_trait_objects,
-    deprecated,
-    explicit_outlives_requirements,
-    noop_method_call,
-    rust_2018_idioms,
-    rust_2021_incompatible_closure_captures,
-    rust_2021_incompatible_or_patterns,
-    rust_2021_prefixes_incompatible_syntax,
-    rust_2021_prelude_collisions,
-    single_use_lifetimes,
-    trivial_casts,
-    trivial_numeric_casts,
-    unused_import_braces,
-    unused_lifetimes,
-    unused_qualifications
-)]
-#![warn(unused_crate_dependencies, unused_extern_crates)]
-//#![warn(missing_docs)] //TODO uncomment eventually
-//#![warn(variant_size_differences)] // enable when working on performance
-//#![allow(dead_code)]
-
 #[cfg(feature = "asynchronous")]
 pub mod asynchronous;
 mod inner_listener;
