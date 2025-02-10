914bd6cc4ce90cd00be0d865718d8c82489d762e
haraldmaida <harald.maida@innoave.com>
2025-02-10 16:16:26 +0100

    remove crate feature `asynchronous`

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,8 @@
 #![doc(html_root_url = "https://docs.rs/output-tracker/0.1.0")]
 
-#[cfg(feature = "asynchronous")]
-pub mod asynchronous;
 mod inner_subject;
 mod inner_tracker;
-#[cfg(any(
-    feature = "non-threadsafe",
-    not(any(feature = "asynchronous", feature = "threadsafe"))
-))]
+#[cfg(any(feature = "non-threadsafe", not(feature = "threadsafe")))]
 pub mod non_threadsafe;
 #[cfg(feature = "threadsafe")]
 pub mod threadsafe;
